e505dd874569d83bef914bae8b3f2c776c3985c6
peterpaw <pb.pawelczyk@gmail.com>
2021-03-25 22:12:40 +0100

    create landing page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,68 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styled from 'styled-components'
+
+const FirstName = styled.h1`
+  text-transform: uppercase;
+  font-size: 1.5rem;
+`
+
+const LastName = styled.h1`
+  margin-bottom: 0.5rem;
+  font-size: 2rem;
+`
+
+const Occupation = styled.h2`
+  border-top: 1px solid #fff;
+  line-height: 1.2;
+`
+
+const Header = styled.div`
+  font-size: 2rem;
+  text-align: right;
+  text-transform: uppercase;
+  padding: 0 1rem;
+  h1,
+  h2 {
+    font-weight: 100;
+  }
+
+  h1 {
+    line-height: 1;
+  }
+
+  p {
+    font-weight: 100;
+    text-transform: none;
+    font-size: 1.5rem;
+  }
+
+  @media screen and (min-width: 600px) {
+    font-size: 3rem;
+  }
+`
+
+const Wrapper = styled.section`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  letter-spacing: 4px;
+`
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Peter Pawelczyk - Web Entwickler</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <Wrapper>
+        <Header>
+          <FirstName>Peter</FirstName>
+          <LastName>Pawelczyk</LastName>
+          <Occupation>Web Entwickler</Occupation>
+          <p>info@peterpaw.de</p>
+        </Header>
+      </Wrapper>
+    </>
   )
 }
